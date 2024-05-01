6a326aefc016bcbad632ad1dbbf39e49d17f952d
Jon Turner <jon.e.turner@outlook.com>
2024-05-01 12:09:52 -0700

    adding ufw, changing output

--- audit_scripts/ubuntu.sh
+++ audit_scripts/ubuntu.sh
@@ -4,16 +4,25 @@ AUDIT_CMD="audit"
 FIX_CMD="fix"
 START_DATE=$(date -u -Iseconds)
 OUTPUT_DIR=$HOME
-FINDINGS_FILE="$OUTPUT_DIR/audit-findings-$START_DATE.json"
+FINDINGS_FILE="$OUTPUT_DIR/audit-findings-$START_DATE.log"
 LOG_FILE="$OUTPUT_DIR/audit-logs-$START_DATE.log"
-ALLOW_SYSTEM_CHANGES=false
+QUIET=false
+# ALLOW_SYSTEM_CHANGES=false
 
 function toLog() {
-    echo "$(date -u -Iseconds): $*" >>"$LOG_FILE"
+    if [ $QUIET ]; then
+        echo "$(date -u -Iseconds): $*" >> "$LOG_FILE"
+    else
+        echo "$(date -u -Iseconds): $*" | tee -a "$LOG_FILE"
+    fi
+}
+
+function toFindings() {
+    echo "$(date -u -Iseconds): $*" >> "$FINDINGS_FILE"
 }
 
 if [ $# -eq 0 ]; then
-    # echo usage instructions
+    echo "USAGE: $0 (audit|fix)"
     exit 1
 fi
 
@@ -30,16 +39,28 @@ case "${command}" in
 esac
 
 if [ "$(id -u)" -ne 0 ]; then
-    echoerr "Error: this script must be run with super-user privileges."
+    toLog "Error: this script must be run with super-user privileges."
     exit 1
 fi
 
+# Check if firewall is configured
+
+if [ "$(which ufw)" ]; then
+    if [ "$(ufw status verbose)" == "Status: inactive" ]; then
+        toLog "ERROR: 'ufw' is disabled!"
+    else
+        toLog "INFO: 'ufw' is enabled."
+    fi
+else
+    toLog "WARNING: Utility 'ufw' is not available."
+fi        
+
 # Check installed services
 
-if service; then
+if [ "$(which service)" ]; then
     service --status-all
 else
-    toLog "WARNING: binary 'service' is not available"
+    toLog "WARNING: Utility 'service' is not available."
 fi
 
 # Check world-writable, executable files
